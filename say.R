08a4a4b9cf1706cfc7fbdc4e38e63a378ab52fa3
Jonathan Schwartz <schwartstack@MacBook-Pro.local>
2026-06-14 10:40:51 -0700

    refactor to make more readable, add documentation, add support for decimals

--- say.R
+++ say.R
@@ -1,49 +1,172 @@
-say<-function(n){
-  if(n==0){
-    return("zero")
-  }else{
-    if(grepl("e",n)){
-      n=as.integer(n)
+SCALE_WORDS = c(
+    "",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+    "sextillion",
+    "septillion",
+    "octillion",
+    "nonillion",
+    "decillion",
+    "undecillion",
+    "duodecillion",
+    "tredecillion",
+    "quattuordecillion",
+    "quindecillion",
+    "sexdecillion",
+    "septendecillion",
+    "octodecillion",
+    "novemdecillion",
+    "vigintillion"
+)
+TENS_PLACE_WORDS = c(
+    "ten",
+    "twenty",
+    "thirty",
+    "fourty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+)
+ONES_PLACE_WORDS = c(
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+)
+
+#' Convert a Number to Its Spoken Representation
+#'
+#' Converts a numeric value into a character string containing its
+#' English-language spoken representation. Whole numbers,
+#' decimal values, negative, and positive numbers are supported.
+#' Decimal values are rendered using the word "point" followed by the
+#' spoken form of each digit in the decimal portion.
+#'
+#' @param n A numeric value to convert.
+#'
+#' @return A character string.
+#'
+#' @examples
+#' say(42)
+#' # "forty-two"
+#'
+#' say(-1234)
+#' # "negative one thousand two hundred thirty-four"
+#'
+#' say(3.1415)
+#' # "three point one four one five"
+#'
+#' say(0.007)
+#' # "zero point zero zero seven"
+say <- function(n) {
+    string_number = as.character(n)
+
+    whole_part = strsplit(string_number, "\\.")[[1]][1]
+    whole_string = .say_whole_number(whole_part)
+
+    decimal_part = strsplit(string_number, "\\.")[[1]][2]
+
+    if (is.na(decimal_part)) {
+        return(whole_string)
     }
-    if(n<0){
-      string="negative"
-      n=-1*n
-    }else{
-      string=""
+    decimal_string = .say_decimal_part(decimal_part)
+
+    return(paste(whole_string, "point", decimal_string))
+}
+
+.say_whole_number <- function(string_number) {
+    if (is.na(string_number)) {
+        return(NA)
     }
-    scale=c("","thousand","million","billion","trillion","quadrillion","quintillion","sextillion","septillion","octillion","nonillion","decillion","undecillion","duodecillion","tredecillion","quattuordecillion","quindecillion","sexdecillion","septendecillion","octodecillion","novemdecillion","vigintillion")
-    tens=c("ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety")
-    ones=c("one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen")
-    
-    s=as.character(n)
-    v=strsplit(s,"")[[1]]
-    v=as.numeric(v)
-    
-    if(length(v)%%3!=0){
-      while(length(v)%%3!=0){
-        v=c(0,v)
-      }
+    if (string_number == "0") {
+        return("zero")
     }
-    m=matrix(v,ncol=3,byrow=T)
-    
-    for(r in 1:nrow(m)){
-      if(m[r,1]>0){
-        string=paste(string,ones[m[r,1]],"hundred")
-      }
-      if(m[r,2]>1){
-        string=paste(string,tens[m[r,2]],ones[m[r,3]])
-        string=paste(trimws(string),rev(scale[1:nrow(m)])[r])
-      }else if(m[r,2]==0&&m[r,3]==0){
-        if(m[r,1]>0){
-        string=paste(trimws(string),rev(scale[1:nrow(m)])[r])
+    if (grepl("^-", string_number)) {
+        string = "negative"
+        string_number = gsub("^-", "", string_number)
+    } else {
+        string = ""
+    }
+    string_digits = strsplit(string_number, "")[[1]]
+    numeric_digits = as.numeric(string_digits)
+    while (length(numeric_digits) %% 3 != 0) {
+        numeric_digits = c(0, numeric_digits)
+    }
+    matrix_number = matrix(numeric_digits, ncol = 3, byrow = T)
+
+    for (row_index in 1:nrow(matrix_number)) {
+        if (matrix_number[row_index, 1] > 0) {
+            string = paste(
+                string,
+                ONES_PLACE_WORDS[matrix_number[row_index, 1]],
+                "hundred"
+            )
         }
-      }else{
-        string=paste(string,ones[10*m[r,2]+m[r,3]])
-        string=paste(trimws(string),rev(scale[1:nrow(m)])[r])
-      }
-      
+        if (matrix_number[row_index, 2] > 1) {
+            string = paste(
+                string,
+                TENS_PLACE_WORDS[matrix_number[row_index, 2]],
+                ONES_PLACE_WORDS[matrix_number[row_index, 3]]
+            )
+            string = paste(
+                trimws(string),
+                rev(SCALE_WORDS[1:nrow(matrix_number)])[row_index]
+            )
+        } else if (
+            matrix_number[row_index, 2] == 0 &&
+                matrix_number[row_index, 3] == 0
+        ) {
+            if (matrix_number[row_index, 1] > 0) {
+                string = paste(
+                    trimws(string),
+                    rev(SCALE_WORDS[1:nrow(matrix_number)])[row_index]
+                )
+            }
+        } else {
+            string = paste(
+                string,
+                ONES_PLACE_WORDS[
+                    10 *
+                        matrix_number[row_index, 2] +
+                        matrix_number[row_index, 3]
+                ]
+            )
+            string = paste(
+                trimws(string),
+                rev(SCALE_WORDS[1:nrow(matrix_number)])[row_index]
+            )
+        }
     }
     return(trimws(string))
-  }
-  
+}
+
+.say_decimal_part <- function(string_decimal_part) {
+    paste(
+        sapply(
+            strsplit(string_decimal_part, "")[[1]],
+            .say_whole_number
+        ),
+        collapse = " "
+    )
 }
